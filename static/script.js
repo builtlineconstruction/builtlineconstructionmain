b375c97659c779b30a51f866d2dd1622b7f9133a
Rajeshwari <rajeshwarirajeshwari34879@gmail.com>
2026-07-21 10:47:23 +0530

    pushing the updated logic in js code

--- static/script.js
+++ static/script.js
@@ -3,41 +3,17 @@ const menuBtn=document.querySelector(".menu-toggle");
 const nav=document.querySelector(".hero-header nav");
 
 menuBtn.onclick=function(){
-
     nav.classList.toggle("show");
-
     if(nav.classList.contains("show")){
-
         menuBtn.innerHTML='<i class="fa-solid fa-xmark"></i>';
-
     }else{
-
         menuBtn.innerHTML='<i class="fa-solid fa-bars"></i>';
-
     }
-
 }
-// document.addEventListener("DOMContentLoaded", function () {
-// const text = [
-//     "BUILD YOUR DREAM HOUSE WITH US",
-//     "QUALITY CONSTRUCTION YOU CAN TRUST",
-//     "WE SHAPE YOUR FUTURE BUILDINGS",
-//     "TRUSTED BY CLIENTS, PROVEN BY PROJECTS",
-//     "BUILT ON TRUST AND QUALITY",
-//     "RELIABLE CONSTRUCTION, PROVEN RESULTS",
-//     "YOUR TRUSTED CONSTRUCTION PARTNER"
-// ];
-// let index = 0;
-// const heroText = document.getElementById("heroText");
-// setInterval(() => {
-//     index = (index + 1) % text.length; 
-//     heroText.textContent = text[index];
-// }, 3000);
-// })
+
 
 document.addEventListener("DOMContentLoaded", function () {
     const heroText = document.getElementById("heroText");
-
     if (heroText) {
         const text = [
             "BUILD YOUR DREAM HOUSE WITH US",
@@ -50,7 +26,6 @@ document.addEventListener("DOMContentLoaded", function () {
         ];
 
         let index = 0;
-
         setInterval(() => {
             index = (index + 1) % text.length;
             heroText.textContent = text[index];
@@ -78,18 +53,18 @@ function startCounting() {
     });
 }
 
-const observer = new IntersectionObserver((entries) => {
+const observes = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             startCounting();
-            observer.unobserve(counterSection);  // ADDED: Only count once
+            observes.unobserve(counterSection);  // ADDED: Only count once
         }
     });
 }, {
     threshold: 0.5
 });
 
-observer.observe(counterSection);
+observes.observe(counterSection);
 
 
 
@@ -123,10 +98,106 @@ document.getElementById("year").textContent = new Date().getFullYear();
 const scrollBtn = document.querySelector(".scroll-btn");
 
 const scrollWrapper = document.querySelector('.scrollWrapper');
-        const items = [...scrollWrapper.children];
+        const item = [...scrollWrapper.children];
 
-        items.forEach((item) => {
+        item.forEach((item) => {
             const clonedItem = item.cloneNode(true);
             // clonedItem.classList.add("red");
             scrollWrapper.appendChild(clonedItem);
 })
+
+const popupForm = document.querySelector('.popup-form')
+const closePopup = document.getElementById('closePopup')
+const openButtons = document.querySelectorAll(".openConsultation");
+
+window.addEventListener("load", function(){
+    if(!sessionStorage.getItem("consultationPopup")){
+         setTimeout(function(){
+            popupForm.classList.add("active");
+            sessionStorage.setItem("consultationPopup","true");
+        },1000);
+    }
+
+    openButtons.forEach(function(button){
+        button.addEventListener('click', function(e){
+            e.preventDefault();
+            popForm.classList.add('active')
+        })
+    })
+
+    closePopup.addEventListener("click", function(){
+        popForm.classList.remove('active')
+    })
+
+    popupForm.addEventListener("click",function(e){
+        if(e.target===popupForm){
+            popupForm.classList.remove("active");
+        }
+    });
+
+})
+document.addEventListener("keydown",function(e){
+    if(e.key==="Escape"){
+        popupForm.classList.remove("active");
+    }
+});
+
+
+
+const reelsSwiper = new Swiper(".reels-slider",{
+
+    slidesPerView:1.3,
+
+    centeredSlides:true,
+
+    spaceBetween:30,
+
+    loop:true,
+
+    speed:700,
+
+    navigation:{
+
+        nextEl:".swiper-button-next",
+
+        prevEl:".swiper-button-prev",
+
+    },
+
+    pagination:{
+
+        el:".swiper-pagination",
+
+        clickable:true,
+
+    },
+
+    breakpoints:{
+
+        320:{
+
+            slidesPerView:1.1,
+
+            spaceBetween:20
+
+        },
+
+        768:{
+
+            slidesPerView:2,
+
+            spaceBetween:25
+
+        },
+
+        1200:{
+
+            slidesPerView:3,
+
+            spaceBetween:30
+
+        }
+
+    }
+
+});
